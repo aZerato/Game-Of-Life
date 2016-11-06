69484fa55a3be48d4afcd7e0c634dffb221081e8
aZerato <azerato.graphe@gmail.com>
2016-11-06 19:13:04 +0100

    Add tempory state color

--- scripts/Main.js
+++ scripts/Main.js
@@ -22,7 +22,8 @@ var Main = (function() {
             i++;
         }
 
-		console.log(self.cellules);
+		// create tempory state.
+		self.AfterStep();
 	};
 
     Main.prototype.initCellules = function() {
@@ -31,7 +32,10 @@ var Main = (function() {
         var celluleX = Math.floor((Math.random() * (self.sizeX - 1)) + 1);
         var celluleY = Math.floor((Math.random() * (self.sizeY - 1)) + 1);
 
-        self.cellules[celluleX][celluleY] = new Cellule(celluleX, celluleY, 1, self.cellules, self.ctx);
+		if (self.cellules[celluleX][celluleY].state == 0)
+		{
+        	self.cellules[celluleX][celluleY] = new Cellule(celluleX, celluleY, 1, self.ctx);
+		}
     };
 
 	Main.prototype.NextStep = function() {
@@ -41,9 +45,79 @@ var Main = (function() {
 		{
 			for(var j = 0; j < self.sizeY; j++) 
 			{
-				self.cellules[i][j].evolution();
+				var cell = self.cellules[i][j];
+				cell.evolution(self.cellules);
 			}
 		}
+		// create tempory state.
+		var changed = self.AfterStep();
+		if(changed == false)
+		{
+			alert('Evolution Max');
+		}
+	};
+
+	Main.prototype.AfterStep = function()
+	{
+		var self = this;
+		var changed = false;
+
+		for (var i = 0; i < self.sizeX; i++)
+		{
+			for(var j = 0; j < self.sizeY; j++) 
+			{
+				var cell = self.cellules[i][j];
+				if(cell.state == 1)
+				{
+					if (cell.step == 0)
+					{
+						cell.changeState(1, self.ctx, "green");
+						changed = true;
+					}
+					else if (cell.step == 1)
+					{
+						cell.changeState(1, self.ctx);
+						changed = true;
+					}
+					
+					// get rounded cells.
+					var arr = [];
+					
+					var cellCornerTopLeft = self.cellules.tryGet(cell.posX - 1, cell.posY + 1, undefined);
+					if (cellCornerTopLeft != undefined && cellCornerTopLeft.state != 0) arr.push(cellCornerTopLeft);
+					var cellCornerTopMiddle = self.cellules.tryGet(cell.posX, cell.posY + 1, undefined);
+					if (cellCornerTopMiddle != undefined && cellCornerTopMiddle.state != 0) arr.push(cellCornerTopMiddle);
+					var cellCornerTopRight = self.cellules.tryGet(cell.posX + 1, cell.posY + 1, undefined);
+					if (cellCornerTopRight != undefined && cellCornerTopRight.state != 0) arr.push(cellCornerTopRight);
+
+					var cellCornerLeft = self.cellules.tryGet(cell.posX - 1, cell.posY, undefined);
+					if (cellCornerLeft != undefined && cellCornerLeft.state != 0) arr.push(cellCornerLeft);
+					var cellCornerRight = self.cellules.tryGet(cell.posX + 1, cell.posY, undefined);
+					if (cellCornerRight != undefined && cellCornerRight.state != 0) arr.push(cellCornerRight);
+
+					var cellCornerBottomLeft = self.cellules.tryGet(cell.posX - 1, cell.posY - 1, undefined);
+					if (cellCornerBottomLeft != undefined && cellCornerBottomLeft.state != 0) arr.push(cellCornerBottomLeft);
+					var cellCornerBottomMiddle = self.cellules.tryGet(cell.posX, cell.posY - 1, undefined);
+					if (cellCornerBottomMiddle != undefined && cellCornerBottomMiddle.state != 0) arr.push(cellCornerBottomMiddle);
+					var cellCornerBottomRight = self.cellules.tryGet(cell.posX + 1, cell.posY - 1, undefined);
+					if (cellCornerBottomRight != undefined && cellCornerBottomRight.state != 0) arr.push(cellCornerBottomRight);
+
+					if (cell.step > 0 && (arr.length < 2 || arr.length > 3))
+					{
+						cell.changeState(1, self.ctx, "red");
+						changed = true;
+					}
+
+					cell.step++;
+				}
+				else
+				{
+					cell.step = 0;
+				}
+			}
+		}
+
+		return changed;
 	};
 
 	return Main;
--- scripts/objects/Cellule.js
+++ scripts/objects/Cellule.js
@@ -4,8 +4,7 @@ var Cellule = (function() {
 		posX, 
 		posY,
         state,
-        cellules,
-		ctx)
+        ctx)
 	{
 		this.typeOf = 'Cellule';
 
@@ -13,10 +12,10 @@ var Cellule = (function() {
 
 		this.posY = posY;
 		
-		// state level : 0 : birth / 1 : life / 2 : unhealthy / 3 : onelife
+		// state level.
 		this.state = 0;
-
-        this.cellules = cellules;
+        
+        this.step = 0;
 
 		// canvas context
 		this.ctx = ctx;
@@ -24,19 +23,20 @@ var Cellule = (function() {
 		this.changeState(state, this.ctx);
 	};
 
-	Cellule.prototype.changeState = function(state, ctx) {
+	Cellule.prototype.changeState = function(state, ctx, color) {
 		var self = this;
 
 		if(state == 1)
 		{
 			self.state = state;
-			self.ctx.fillStyle = "blue";
+			self.ctx.fillStyle = color ? color : "blue";
 			self.ctx.fillRect(self.posX * 10, self.posY * 10, 10, 10);
             self.ctx.strokeStyle = "black";
 			self.ctx.strokeRect(self.posX * 10, self.posY * 10, 10, 10);
 		}
         else if(state == 0)
 		{
+            self.step = 0;
 			self.state = state;
             self.ctx.fillStyle = "white";
             self.ctx.fillRect(self.posX * 10, self.posY * 10, 10, 10);
@@ -45,41 +45,38 @@ var Cellule = (function() {
 		}
 	};
 
-    Cellule.prototype.evolution = function() {
+    Cellule.prototype.evolution = function(cells) {
         var self = this;
 
         // get rounded cells.
         var arr = [];
         
-        var cellCornerTopLeft = self.cellules.tryGet(self.posX - 1, self.posY + 1, undefined);
+        var cellCornerTopLeft = cells.tryGet(self.posX - 1, self.posY + 1, undefined);
         if (cellCornerTopLeft != undefined && cellCornerTopLeft.state != 0) arr.push(cellCornerTopLeft);
-        var cellCornerTopMiddle = self.cellules.tryGet(self.posX, self.posY + 1, undefined);
+        var cellCornerTopMiddle = cells.tryGet(self.posX, self.posY + 1, undefined);
         if (cellCornerTopMiddle != undefined && cellCornerTopMiddle.state != 0) arr.push(cellCornerTopMiddle);
-        var cellCornerTopRight = self.cellules.tryGet(self.posX + 1, self.posY + 1, undefined);
+        var cellCornerTopRight = cells.tryGet(self.posX + 1, self.posY + 1, undefined);
         if (cellCornerTopRight != undefined && cellCornerTopRight.state != 0) arr.push(cellCornerTopRight);
 
-        var cellCornerLeft = self.cellules.tryGet(self.posX - 1, self.posY, undefined);
+        var cellCornerLeft = cells.tryGet(self.posX - 1, self.posY, undefined);
         if (cellCornerLeft != undefined && cellCornerLeft.state != 0) arr.push(cellCornerLeft);
-        var cellCornerRight = self.cellules.tryGet(self.posX + 1, self.posY, undefined);
+        var cellCornerRight = cells.tryGet(self.posX + 1, self.posY, undefined);
         if (cellCornerRight != undefined && cellCornerRight.state != 0) arr.push(cellCornerRight);
 
-        var cellCornerBottomLeft = self.cellules.tryGet(self.posX - 1, self.posY - 1, undefined);
+        var cellCornerBottomLeft = cells.tryGet(self.posX - 1, self.posY - 1, undefined);
         if (cellCornerBottomLeft != undefined && cellCornerBottomLeft.state != 0) arr.push(cellCornerBottomLeft);
-        var cellCornerBottomMiddle = self.cellules.tryGet(self.posX, self.posY - 1, undefined);
+        var cellCornerBottomMiddle = cells.tryGet(self.posX, self.posY - 1, undefined);
         if (cellCornerBottomMiddle != undefined && cellCornerBottomMiddle.state != 0) arr.push(cellCornerBottomMiddle);
-        var cellCornerBottomRight = self.cellules.tryGet(self.posX + 1, self.posY - 1, undefined);
+        var cellCornerBottomRight = cells.tryGet(self.posX + 1, self.posY - 1, undefined);
         if (cellCornerBottomRight != undefined && cellCornerBottomRight.state != 0) arr.push(cellCornerBottomRight);
 
-        if(arr.length == 2)
-        {
-            //stay.
-        }
-        if ( arr.length == 3) 
+        if (arr.length == 3) 
         {
             self.changeState(1);
         }
         else if (arr.length < 2 || arr.length > 3)
         {
+            self.step = 0;
             self.changeState(0);
         }
     };
